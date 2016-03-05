f7e8a087e62b2145fbfcab769e23e4572e908898
swaff <mdswaffer@gmail.com>
2016-03-05 20:27:05 +0000

    Starts the process of displaying the sums
    Adds a rough idea of the style for the sums. Needs more work though.

--- assets/javascript/controllers/club.js
+++ assets/javascript/controllers/club.js
@@ -1,3 +1,11 @@
-module.exports = ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.number = $routeParams.number;
+module.exports = ['$routeParams', '$http', function ($routeParams, $http) {
+
+    this.number = $routeParams.number;
+
+    $http.get(`/api/sums?number=${this.number}`)
+        .success((data) => this.sums = data.sums);
+
+    this.hasStarted = false;
+    this.start = () => this.hasStarted = true;
+
 }];
