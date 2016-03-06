86e8d8bb8f7e4f563ca81ae56f0695e6ba9f0c45
swaff <mdswaffer@gmail.com>
2016-03-06 22:51:39 +0000

    Adds the operator sign to the sums

--- assets/javascript/controllers/club.js
+++ assets/javascript/controllers/club.js
@@ -1,9 +1,24 @@
+const operatorMap = {
+    addition: '+',
+    subtraction: '-',
+    multiplication: '×',
+    division: '÷'
+};
+
+const decorateSum = (sum) => {
+    sum.sign = operatorMap[sum.operator];
+    return sum;
+};
+
 module.exports = ['$routeParams', '$http', function ($routeParams, $http) {
 
     this.number = $routeParams.number;
+    this.sums = [];
 
     $http.get(`/api/sums?number=${this.number}`)
-        .success((data) => this.sums = data.sums);
+        .success((data) => {
+            this.sums = data.sums.map(decorateSum);
+        });
 
     this.hasStarted = false;
     this.start = () => this.hasStarted = true;
